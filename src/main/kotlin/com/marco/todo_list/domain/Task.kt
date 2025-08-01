0a0a0e214de8915ae24eec23d04e3aceecf368dc
AurelioMarcoRibeiro <marcoaurelio20010514@gmail.com>
2025-08-01 01:28:55 -0300

    add Docker Compose, create task table via migration, and initialize DB

---
+++ src/main/kotlin/com/marco/todo_list/domain/Task.kt
@@ -0,0 +1,33 @@
+package com.marco.todo_list.domain
+
+import com.github.f4b6a3.uuid.UuidCreator
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import org.hibernate.Hibernate
+import java.math.BigDecimal
+import java.time.LocalDate
+
+@Entity
+data class Task(
+    @Id val id: String = UuidCreator.getTimeOrderedEpoch().toString(),
+
+    var name: String? = null,
+    var cost: BigDecimal? = null,
+    var dueDate: LocalDate? = null,
+    var displayOrder: Int? = null
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as Task
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
+
+    override fun toString(): String {
+        return "Task(id='$id', name='$name', cost='$cost', dueDate=$dueDate, displayOrder=$displayOrder)"
+    }
+}
