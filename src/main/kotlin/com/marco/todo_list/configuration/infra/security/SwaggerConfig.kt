eec952a1f4f6cf430715bcc09216bf16eec1d900
AurelioMarcoRibeiro <marcoaurelio20010514@gmail.com>
2025-08-04 02:12:52 -0300

    fix https error on swagger

--- src/main/kotlin/com/marco/todo_list/configuration/infra/security/SwaggerConfig.kt
+++ src/main/kotlin/com/marco/todo_list/configuration/infra/security/SwaggerConfig.kt
@@ -1,5 +1,6 @@
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,7 +10,9 @@ class SwaggerConfig {
 
     @Bean
     fun customOpenAPI(): OpenAPI {
+        val server = Server().url("https://todo-list-be-production-5c1f.up.railway.app")
         return OpenAPI()
+            .addServersItem(server)
             .info(
                 Info()
                     .title("Todo List API")
